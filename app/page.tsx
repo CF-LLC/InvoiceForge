c10df31cbaa778b8e6d8ebabdc6dba6786f74830
Cooper Featherstone <44649389+coopfeathy@users.noreply.github.com>
2025-03-22 22:38:17 -0500

    Aop

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import InvoiceGeneratorApp from "@/components/invoice-generator-app"
+
+export default function Home() {
+  return (
+    <main>
+      <InvoiceGeneratorApp />
+    </main>
+  )
+}
+
